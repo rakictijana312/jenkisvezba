309c8e00a20ab570021af0ada5b05ac6cf10365e
rakic <rakicijana312@gmail.com>
2023-06-16 16:53:50 -0400

    smoke runner

--- src/test/java/Main.java
+++ src/test/java/Main.java
@@ -1,4 +1,12 @@
+import org.junit.Test;
+
 public class Main {
 
-
+    @Test
+    public static void main(String[] args) throws InterruptedException {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Batch 9 is great");
+            Thread.sleep(1000);
+        }
+    }
 }
